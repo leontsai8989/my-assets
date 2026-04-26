eb1d7f2406df528b5dbdc5b28ef810cb7e8a2bc0
leontsai8989 <tobytsai8926@gmail.com>
2026-04-26 15:01:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open('portfolio-v1').then((cache) => {
-            return cache.addAll([
-                'assets/index.html',
-                'stocks/index.html',
-                'manifest.json'
-            ]);
-        })
-    );
+  e.waitUntil(
+    caches.open('portfolio-v1').then((cache) => {
+      return cache.addAll([
+        'assets/index.html',
+        'stocks/index.html',
+        'manifest.json'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
